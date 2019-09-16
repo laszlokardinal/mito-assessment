c7f2d7b300b0aa865d6c8923a6aa737f7104e7ba
László Kardinál <laszlo.kardinal@gmail.com>
2019-09-16 03:22:41 +0200

    Moved SubmitButton from components to elements

--- frontend/src/view/components/index.js
+++ frontend/src/view/components/index.js
@@ -3,4 +3,3 @@ export DropdownInputField from "./DropdownInputField.vue";
 export HomeCard from "./HomeCard.vue";
 export SelectFlightTitle from "./SelectFlightTitle.vue";
 export SelectFlightHeader from "./SelectFlightHeader.vue";
-export SubmitButton from "./SubmitButton.vue";
--- frontend/src/view/elements/index.js
+++ frontend/src/view/elements/index.js
@@ -1,3 +1,4 @@
 export InputField from "./InputField.vue";
 export DatePicker from "./DatePicker.vue";
 export DropdownList from "./DropdownList.vue";
+export SubmitButton from "./SubmitButton.vue";
