2ea0f147fb564608b88c42ef1ebdaa7116b19708
László Kardinál <laszlo.kardinal@gmail.com>
2019-09-16 03:36:56 +0200

    Added FlightsSeparator element

---
+++ frontend/src/view/elements/FlightsSeparator.vue
@@ -0,0 +1,48 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <div class="flights-separator" />
+</template>
+
+<style lang="scss">
+@import "~/view/variables.scss";
+
+.flights-separator {
+  height: 0px;
+
+  background-color: $border-gray;
+  border-top: 1px dashed $gray;
+
+  position: relative;
+
+  &:before {
+    content: " ";
+
+    position: absolute;
+    top: -5px;
+    left: calc(50% - 5px);
+    width: 9px;
+    height: 9px;
+
+    border-right: 1px solid $gray;
+    border-bottom: 1px solid $gray;
+
+    transform: rotate(45deg);
+
+    background-color: $white;
+  }
+
+  &:after {
+    content: " ";
+
+    position: absolute;
+    top: -1px;
+    left: calc(50% - 6px);
+    width: 12px;
+
+    border-top: 1px solid $border-gray;
+  }
+}
+</style>
--- frontend/src/view/elements/index.js
+++ frontend/src/view/elements/index.js
@@ -1,5 +1,6 @@
-export InputField from "./InputField.vue";
 export DatePicker from "./DatePicker.vue";
 export DropdownList from "./DropdownList.vue";
 export FlightsItem from "./FlightsItem.vue";
+export FlightsSeparator from "./FlightsSeparator.vue";
+export InputField from "./InputField.vue";
 export SubmitButton from "./SubmitButton.vue";
