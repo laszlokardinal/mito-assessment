7c752b853d675705621dec1cdf2f3727d7f9aab1
László Kardinál <laszlo.kardinal@gmail.com>
2019-09-17 06:54:03 +0200

    Added logger to vuex in development mode

--- frontend/src/store/createStore.js
+++ frontend/src/store/createStore.js
@@ -1,8 +1,18 @@
 import Vuex from "vuex";
+import createLogger from "vuex/dist/logger";
 
-const createStore = () =>
-  new Vuex.Store({
-    modules: {}
+const createStore = () => {
+  const plugins = [];
+
+  if (process.env.NODE_ENV === "development") {
+    plugins.push(createLogger());
+  }
+
+  return new Vuex.Store({
+    modules: {
+    },
+    plugins
   });
+};
 
 export default createStore;
